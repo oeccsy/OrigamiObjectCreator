6bdbd9e84e85963ab97482536e90a8e1b942444b
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-04-27 19:02:30 +0900

    test : edge 선택

--- DevOnly/Study/obj_con_code.py
+++ DevOnly/Study/obj_con_code.py
@@ -1,9 +1,11 @@
 import bpy
+import bmesh
 
 ### obj 선택
 
 def select_obj(name="NewObj"):
   bpy.data.objects[name].select_set(True)
+  return bpy.data.objects[name]
   
 # active_object는 단일 오브젝트를 나타내고,
 # selected_objects는 여러 오브젝트의 목록을 나타낸다.
@@ -20,3 +22,34 @@ def get_active_obj():
 
 def get_selected_objs():
   return bpy.context.selected_objects
+
+
+### edge 선택
+# bmesh 모듈을 이용
+# bmesh : 복잡한 메시 조작 작업을 수행하는 데 사용하는 모듈
+
+def select_edge(index_list=[]):
+  obj = bpy.context.active_object
+  
+  bpy.ops.object.mode_set(mode='EDIT')
+  bpy.ops.mesh.select_all(action='DESELECT')
+  
+  # 메시 데이터를 수정하기 위해 bmesh 불러오기
+  bm = bmesh.from_edit_mesh(obj.data)
+  
+  # 모든 선택 해제
+  bm.select_flush(False)
+  
+  # 엣지 선택
+  for index in index_list:
+    bm.edges[index].select = True
+
+# 변경된 선택 사항을 메시에 적용
+  bmesh.update_edit_mesh(obj.data)
+  
+  # 메모리 해제
+  bm.free()
+  
+  bpy.ops.object.mode_set(mode='OBJECT')
+  
+  
