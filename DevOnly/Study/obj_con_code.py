fc4b988b2f110d3987e2ae9c95b96831e5c69d2a
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-04-28 19:21:35 +0900

    test : subdivide edge

--- DevOnly/Study/obj_con_code.py
+++ DevOnly/Study/obj_con_code.py
@@ -79,4 +79,64 @@ def select_edge(index_list=[]):
   # 메모리 해제
   bm.free()
   
+
+### subdivide
+def subdivide_edges(index_list=[]):
+  bpy.ops.object.mode_set(mode = 'OBJECT')
+  obj = bpy.context.active_object
+  
+  bpy.ops.object.mode_set(mode='EDIT')
+  bpy.context.tool_settings.mesh_select_mode = (True, False, False)
+  bpy.ops.mesh.select_all(action='DESELECT')
+  
+  # 메시 데이터를 수정하기 위해 bmesh 불러오기
+  bm = bmesh.from_edit_mesh(obj.data)
+  
+  # 모든 선택 해제
+  bm.select_flush(False)
+  bm.edges.ensure_lookup_table()
+  
+  selected_edges = []
+
+  # 엣지 선택
+  for index in index_list:
+    bm.edges[index].select_set(True)
+    selected_edges.append(bm.edges[index])
+
+  bmesh.ops.subdivide_edges(bm,
+                            edges=selected_edges,
+                            cuts=1,
+                            use_grid_fill=True,
+                            )     
+  
+  # 변경된 선택 사항을 메시에 적용
+  bmesh.update_edit_mesh(obj.data)
+  
+  # 메모리 해제
+  bm.free()
+  
+
+def subdivide_edge(index):
+  bpy.ops.object.mode_set(mode = 'OBJECT')
+  obj = bpy.context.active_object
+
+  bpy.ops.object.mode_set(mode='EDIT')
+  bpy.ops.mesh.select_all(action='DESELECT')
+  bpy.context.tool_settings.mesh_select_mode = (True, False, False)
+
+  bm = bmesh.from_edit_mesh(obj.data)
+
+  bm.select_flush(False)
+  bm.edges.ensure_lookup_table()
+
+  bm.edges[index].select_set(True)
+  bmesh.update_edit_mesh(obj.data) 
+
+  bmesh.ops.subdivide_edges(bm,
+                            edges=[bm.edges[index]],
+                            cuts=1,
+                            use_grid_fill=True,
+                            )                          
+  bmesh.update_edit_mesh(obj.data) 
+  bm.free()
   
