9c8f87270df5176e84b7ee4bd5a1cfffab442db2
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-05-09 02:31:44 +0900

    feat : main

--- Program/main.py
+++ Program/main.py
@@ -1,3 +1,8 @@
 import Models
+import Modules
 
-Models.create_papercrane()
+if __name__ == '__main__':
+  Modules.blender_utils.init_objects_data()
+  Modules.blender_utils.create_new_plane()
+  Models.create_papercrane()
+  Modules.blender_utils.fbx_export("paper_crane")    
