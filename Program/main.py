635d63b10ca094ebe603b3a8c44f6aa6a9152c05
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-05-09 22:35:32 +0900

    feat : paper airplane export

--- Program/main.py
+++ Program/main.py
@@ -7,3 +7,9 @@ if __name__ == '__main__':
   Models.create_paper_crane()
   Modules.blender_utils.fbx_export("paper_crane")    
   Modules.blender_utils.stl_export("paper_crane") 
+  
+  Modules.blender_utils.init_objects_data()
+  Modules.blender_utils.create_new_plane()
+  Models.create_paper_airplane()
+  Modules.blender_utils.fbx_export("paper_airplane")    
+  Modules.blender_utils.stl_export("paper_airplane") 
